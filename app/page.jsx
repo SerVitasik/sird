840ace5ed7fd7262609dee02147d4f7368a347d9
Vitalii Kulyk <vitaliy.kulik2313@gmail.com>
2023-09-05 19:03:46 +0300

    Main page was updated

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,12 @@
 import styles from '../styles/page.module.scss';
 import Header from '../components/Header';
+import Sird from '@/components/Sird';
 export default function HomePage() {
   return (
     <>
       <Header />
       <main className={styles.main}>
-        hello
-        
-        
+        <Sird/>
       </main>
       {/* <MainComponent>
       hello
---
+++ components/Sird.jsx
@@ -0,0 +1,11 @@
+import styles from '../styles/components/Sird.module.scss';
+
+const Sird = () => {
+    return (
+        <section className={styles.Sird}>
+            <h1 className={styles.Title}>СІВЕРСЬКИЙ ІНСТИТУТ РЕГІОНАЛЬНИХ ДОСЛІДЖЕНЬ</h1>            
+        </section>
+    );
+};
+
+export default Sird;
