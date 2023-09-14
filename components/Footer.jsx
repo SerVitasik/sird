cdfbbce87238e879dc8be05147efb83379fd533a
Vitalii Kulyk <vitaliy.kulik2313@gmail.com>
2023-09-14 20:37:17 +0300

    footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 
 const Footer = () => {
-    return (
-        <footer>
-            <Image />
-        </footer>
-    );
+  return (
+    <footer>
+      <Image />
+      Hello sexik!
+    </footer>
+  );
 };
 
 export default Footer;
--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,20 +1,33 @@
 import Link from "next/link";
 import styles from "../styles/components/Navigation.module.scss";
 const Navigation = () => {
-    return (
-        <nav className={styles.Navigation}>
-            <ul className={styles.List}>
-                <li className={styles.Link}><Link href="/">Головна</Link></li>
-                <li className={styles.Link}><Link href="/news">Новини</Link></li>
-                <li className={styles.Link}><Link href="/news">Контакти</Link></li>
-                <li className={styles.Link}><Link href="/news">Звіти</Link></li>
-                <li className={styles.Link}><Link href="/news">Розробки</Link></li>
-                <li className={styles.Link}><Link href="/news">Про нас</Link></li>
-                <li className={styles.Link}><Link href="/news">Проєкти</Link></li>
-               
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className={styles.Navigation}>
+      <ul className={styles.List}>
+        <li className={styles.Link}>
+          <Link href="/">Головна</Link>
+        </li>
+        <li className={styles.Link}>
+          <Link href="/news">Новини</Link>
+        </li>
+        <li className={styles.Link}>
+          <Link href="/news">Контакти</Link>
+        </li>
+        <li className={styles.Link}>
+          <Link href="/news">Звіти</Link>
+        </li>
+        <li className={styles.Link}>
+          <Link href="/news">Розробки</Link>
+        </li>
+        <li className={styles.Link}>
+          <Link href="/news">Про нас</Link>
+        </li>
+        <li className={styles.Link}>
+          <Link href="/news">Проєкти</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navigation;
