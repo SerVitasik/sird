33db02c4d62d87cc822385862077dd4bf8cd27d7
Vitalii Kulyk <vitaliy.kulik2313@gmail.com>
2023-09-26 22:16:08 +0300

    Added news detail page

--- app/news/[newId]/page.jsx
+++ app/news/[newId]/page.jsx
@@ -1,14 +1,23 @@
+"use client"
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Wrapper from "@/components/ui/Wrapper";
 import MainComponent from "@/components/ui/MainComponent";
+import NewsDetail from "@/components/NewsDetail";
+import { News } from "@/store/news";
+import { usePathname } from "next/navigation";
+const NewsDetailPage = () => {
 
-const NewsDetailPage = (props) => {
+  const pathname = usePathname();
+  const currentId = pathname.split("/").pop();
+console.log(currentId);
+  const currentNews = News.find(item => item.id === currentId);
+  console.log(currentNews);
   return (
     <Wrapper>
       <Header />
       <MainComponent>
-        <NewsDetail />
+        <NewsDetail news={currentNews}/>
       </MainComponent>
       <Footer />
     </Wrapper>
--- components/NewsDetail.jsx
+++ components/NewsDetail.jsx
@@ -1,5 +1,21 @@
+import Image from "next/image";
+
 const NewsDetail = (props) => {
-  return <>Title {props.title}</>;
+
+  const formatedDate = `${props.news.date.getDate()}.${props.news.date.getMonth() + 1}.${
+    props.news.date.getFullYear()
+  }`;
+
+  return <section>
+  <h1>{props.news.title}</h1>
+  <Image src={props.news.image}
+                        alt="News item"
+                        width={1300}
+                        height={543}
+                        />
+  <div>{formatedDate}</div>
+  <p>{props.news.text}</p>
+  </section>;
 };
 
 export default NewsDetail;
