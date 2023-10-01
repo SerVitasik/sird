572bcbcedaf1c8a6f68870c618ad2ea113577835
Vitalii Kulyk <vitaliy.kulik2313@gmail.com>
2023-10-01 17:42:20 +0300

    added burger menu and header, footer, sird, news were adapted for phones

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,32 +1,56 @@
+"use client"
 import Link from "next/link";
 import styles from "./Navigation.module.scss";
+import { useState } from "react";
 const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [hideOrShow, setHideOrShow] = useState({});
+
+  const menuHandler = () => {
+    setIsOpen(prev => !prev);
+    if (isOpen) {
+        setHideOrShow(() => {
+          return {};
+        });
+    } else {
+      setHideOrShow(() => {
+        return {left: "0"};
+      });
+    }
+  };
   return (
-    <nav className={styles.Navigation}>
-      <ul className={styles.List}>
-        <li className={styles.Link}>
-          <Link href="/">Головна</Link>
-        </li>
-        <li className={styles.Link}>
-          <Link href="/news">Новини</Link>
-        </li>
-        <li className={styles.Link}>
-          <Link href="/contacts">Контакти</Link>
-        </li>
-        <li className={styles.Link}>
-          <Link href="/reports">Звіти</Link>
-        </li>
-        <li className={styles.Link}>
-          <Link href="/developments">Розробки</Link>
-        </li>
-        <li className={styles.Link}>
-          <Link href="/aboutUs">Про нас</Link>
-        </li>
-        <li className={styles.Link}>
-          <Link href="/projects">Проєкти</Link>
-        </li>
-      </ul>
-    </nav>
+    <div className={styles.navigation}>
+      {isOpen ? <div onClick={menuHandler} className={styles["button","button-open"]}>
+        <span></span>
+      </div> : <div onClick={menuHandler} className={styles.button}>
+        <span></span>
+      </div>}
+      <nav className={styles["body","vision"]} style={hideOrShow}>
+        <ul className={styles.list}>
+          <li className={styles.link}>
+            <Link href="/">Головна</Link>
+          </li>
+          <li className={styles.link}>
+            <Link href="/news">Новини</Link>
+          </li>
+          <li className={styles.link}>
+            <Link href="/contacts">Контакти</Link>
+          </li>
+          <li className={styles.link}>
+            <Link href="/reports">Звіти</Link>
+          </li>
+          <li className={styles.link}>
+            <Link href="/developments">Розробки</Link>
+          </li>
+          <li className={styles.link}>
+            <Link href="/aboutUs">Про нас</Link>
+          </li>
+          <li className={styles.link}>
+            <Link href="/projects">Проєкти</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
