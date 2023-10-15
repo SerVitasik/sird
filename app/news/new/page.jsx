f33f98cc06e0f4756c0abe7386603b6162d4bdaf
Vitalii Kulyk <vitaliy.kulik2313@gmail.com>
2023-10-15 21:38:15 +0300

    Fixed bag with add news"

--- app/news/new/page.jsx
+++ app/news/new/page.jsx
@@ -1,22 +1,25 @@
+"use client";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Wrapper from "@/components/ui/Wrapper";
 import MainComponent from "@/components/ui/MainComponent";
 import NewNewsForm from "@/components/NewNewsForm";
+import { useRouter } from "next/navigation";
 
 const NewNewsPage = () => {
-
+  const router = useRouter();
   async function addMeetupHandler(enteredMeetupData) {
-    const response = await fetch('/api/new', {
-      method: 'POST',
+    const response = await fetch("/api/news/new", {
+      method: "POST",
       body: JSON.stringify(enteredMeetupData),
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     });
 
     const data = await response.json();
     console.log(data);
+    router.replace("/");
   }
 
   return (
