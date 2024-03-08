e6a4a48e71dfe09391e42a9975d82603e390d209
Vitalii Kulyk <vitaliy.kulik2313@gmail.com>
2024-03-08 16:48:47 +0200

    Fixed issue with LoginForm

--- components/auth/LoginForm.jsx
+++ components/auth/LoginForm.jsx
@@ -1,64 +1,61 @@
 "use client";
-import styles from './LoginForm.module.scss';
-import { useRef, useState } from 'react';
+import styles from "./LoginForm.module.scss";
+import { useRef, useState } from "react";
 import { signIn } from "next-auth/react";
-import { useRouter } from 'next/navigation';
-import { login } from '@/store';
-import { useDispatch } from 'react-redux';
+import { useRouter } from "next/navigation";
+
 const LoginForm = () => {
-    const [error, setError] = useState("");
-    const usernameInputRef = useRef();
-    const passwordInputRef = useRef();
-    const router = useRouter();
-    const dispatch = useDispatch();
-  
-    async function loginHandler(enteredUserData) {
-      try {
-        const res = await signIn('credentials', {
-          username: enteredUserData.username,
-          password: enteredUserData.password,
-          redirect: false
-        });
-        if (res.error) {
-         setError("Incorrect password");
-         return;
-        }
-        dispatch(login());
-       router.push("/"); 
-      } catch (error) {  
-        console.error(error);
+  const [error, setError] = useState("");
+  const usernameInputRef = useRef();
+  const passwordInputRef = useRef();
+  const router = useRouter();
+
+  async function loginHandler(enteredUserData) {
+    try {
+      const res = await signIn("credentials", {
+        username: enteredUserData.username,
+        password: enteredUserData.password,
+        redirect: false,
+      });
+      if (res.error) {
+        setError("Incorrect password");
+        return;
       }
+      router.push("/");
+    } catch (error) {
+      console.error(error);
     }
+  }
 
-    function submitHandler(event) {
-      event.preventDefault();
-  
-      const enteredUsername = usernameInputRef.current.value;
+  function submitHandler(event) {
+    event.preventDefault();
 
-      const enteredPassword= passwordInputRef.current.value;
-      const userData = {
-        username: enteredUsername,
-        password: enteredPassword,
-      };
-  
-      loginHandler(userData);
-    }
-  
-    return (
-        <form className={styles.form} onSubmit={submitHandler}>
-          <div className={styles.control}>
-            <label htmlFor='login'>Логін</label>
-            <input type='text' required id='username' ref={usernameInputRef} />
-          </div>
-         
-          <div className={styles.control}>
-            <label htmlFor='password'>Пароль</label>
-            <input type='password' required id='password' ref={passwordInputRef} />
-          </div>
-            {error && <p className={styles.error}>{error}</p>}
-            <button className={styles.button}>Увійти</button>
-        </form>
-    );
+    const enteredUsername = usernameInputRef.current.value;
+
+    const enteredPassword = passwordInputRef.current.value;
+    const userData = {
+      username: enteredUsername,
+      password: enteredPassword,
+    };
+
+    loginHandler(userData);
+  }
+
+  return (
+    <form className={styles.form} onSubmit={submitHandler}>
+      <div className={styles.control}>
+        <label htmlFor="login">Логін</label>
+        <input type="text" required id="username" ref={usernameInputRef} />
+      </div>
+
+      <div className={styles.control}>
+        <label htmlFor="password">Пароль</label>
+        <input type="password" required id="password" ref={passwordInputRef} />
+      </div>
+      {error && <p className={styles.error}>{error}</p>}
+      <button className={styles.button}>Увійти</button>
+    </form>
+  );
 };
 
 export default LoginForm;
